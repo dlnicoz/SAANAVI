96c4bc8a8554d14e9fe9905958a95f4cc358bdfc
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2022-11-11 22:51:24 +0530

    clickable contact number

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "../css/Contact.css";
 // pending work probably doing after jan
 
+
 function Contact() {
   return (
     <>
       <div className="contact-wrap">
         <div className="contact-div">
           <div className="contact-inner-div">
-            <p className="p1">I am Nilesh </p>
+            <p className="p1">I am Nilesh Kadam </p>
             <p className="p2">owner and manager of saanvi</p>
-            <p className="p3">contact me on whatsapp for booking</p>
+            <a className="link" href="tel:+919420030574"><p className="p3">contact me +91 9420030574</p></a>
           </div>
         </div>
       </div>
       <a
-        href="https://api.whatsapp.com/send?phone=917304861722"
+        href="https://api.whatsapp.com/send?phone=919420030574"
         className="whatsapp" target="_blank" rel="noreferrer noopener"
       >
         <i className="fa fa-whatsapp whatsapp-icon"></i>
