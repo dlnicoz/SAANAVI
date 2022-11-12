bf478cdbc9c8b1256311b95e22798040f18698b4
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2022-11-12 15:51:41 +0530

    try hiding animation on navbar to fix white space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,17 +23,17 @@ function Navbar() {
 
     gsap.fromTo(".hero-clipped", { scaleX: 0 }, { duration: 1, scaleX: 1 });
 
-    gsap.fromTo(
-      ".logo",
-      { x: -202, opacity: 0 },
-      { duration: 1, delay: 0.5, x: 0, opacity: 1 }
-    );
+    // gsap.fromTo(
+    //   ".logo",
+    //   { x: -202, opacity: 0 },
+    //   { duration: 1, delay: 0.5, x: 0, opacity: 1 }
+    // );
 
-    gsap.fromTo(
-      ".hamburger-menu",
-      { x: 200, opacity: 0 },
-      { duration: 1, delay: 1, x: 0, opacity: 1 }
-    );
+    // gsap.fromTo(
+    //   ".hamburger-menu",
+    //   { x: 200, opacity: 0 },
+    //   { duration: 1, delay: 1, x: 0, opacity: 1 }
+    // );
 
     gsap.fromTo(
       ".hero-textbox",
