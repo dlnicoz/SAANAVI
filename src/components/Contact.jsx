818a7af18f32b786056e4309302565159e94b800
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2022-11-14 19:23:36 +0530

    re contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,60 @@
 import React from "react";
 import "../css/Contact.css";
-import ww from "./images/www.svg"
+import nilesh from "./images/nilesh.jpg";
 // pending work probably doing after jan
-
 
 function Contact() {
   return (
     <>
-      <div className="contact-wrap">
-        <div className="contact-div">
-          <div className="contact-inner-div">
-            <p className="p1">I am Nilesh Kadam </p>
-            <p className="p2">owner and manager of saanvi</p>
-            <a className="link" href="tel:+919420030574"><p className="p3">contact me +91 9420030574</p></a>
+      <div className="ccontainer">
+        <div className="cinfo">
+          <div className="top1">
+          <h1 className="ch1">Nilesh kadam</h1>
+          <h2 className="ch2">owner of saanvi hotel</h2>
+          <p className="cspan">
+            <em> "Go to nature to get your stolen soul back"</em>
+          </p>
           </div>
+          <div className="top2">
+          <a className="link" href="tel:+919420030574">
+            <p className="p3">contact me here</p></a>
+            
+            <button
+              className="sharer"
+              data-sharer="whatsapp"
+              data-title="Merida Design Blog"
+              data-url="https://api.whatsapp.com/send?phone=919420030574"
+            >
+              {/* <i className="fa fa-whatsapp"></i> */}
+              WhatsApp
+            </button>
+            <button
+              className="sharer"
+              data-sharer="phone"
+              data-title="Merida Design Blog"
+              data-url="tel:+919420030574"
+            >
+              {/* <i className="fa fa-phone"></i> */}
+              Phone
+            </button>
+          
+          
+          </div>
+         
+
+         
+
+          {/* <a
+            href="https://api.whatsapp.com/send?phone=919420030574"
+            className="whatsapp"
+            target="_blank"
+            rel="noreferrer noopener"
+          ></a> */}
+        </div>
+        <div className="cimg">
+          <img className="nileshimg" src={nilesh} alt="nilesh kadam" />
         </div>
       </div>
-      <a
-        href="https://api.whatsapp.com/send?phone=919420030574"
-        className="whatsapp" target="_blank" rel="noreferrer noopener"
-      >
-        <img className="whatsapp-icon" src={ww}></img>
-      </a>
     </>
   );
 }
