8e540aad8bd40699187489b4b2f1adc429f3164c
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2022-12-07 15:48:23 +0530

    final whatsapp

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../css/Wh.css'
-
+import wh from "../components/images/www.svg"
 function Whatsapp(){
 
     return(<>
 <div className="floating_btn">
-    <a className="wha"target="_blank" rel='noreferrer' href="https://wa.me/">
+    <a className="wha"target="_blank" rel='noreferrer' href="https://api.whatsapp.com/send?phone=919420030574">
       <div className="contact_icon">
-        <i className="fa fa-whatsapp my-float"></i>
+        {/* <i className="fas fa-whatsapp my-float large-icon"></i> */}
+        <img src={wh} alt="z"  />
       </div>
     </a>
     <p className="text_icon">Talk to us!</p>
